077685185261a23b72c641bb5bc307d26b06c913
Tyan89@list.ru <Tyan89@list.ru>
2020-12-08 16:09:16 +0300

    first

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <img src="https://source.unsplash.com/random/400x400" alt=""/>
+    </>
+  )
+}
+
+export default Header;
