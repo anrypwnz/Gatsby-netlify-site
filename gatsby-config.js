2fee73405bdda799ef6e1a5b3bc38c83f3dd211f
Tyan89@list.ru <Tyan89@list.ru>
2020-12-08 16:38:32 +0300

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,22 +7,6 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: `${__dirname}/src/images/`,
-      },
-      __key: "images",
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      }
-    },
-    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `./src/utils/typography`,
