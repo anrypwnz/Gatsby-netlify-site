b05532f34c2325c878609ac69fa02ef27e9644fa
anrypwnz <anrypwnz@gmail.com>
2020-12-09 17:03:34 +0300

    working

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,10 @@ import * as React from "react";
 import Header from "../components/header";
 import Layout from "../components/layout";
 
+//crate layout for blocks
+//add header
+//add footer
+//add carousel
 const IndexPage = () => {
   return (
     <Layout>
